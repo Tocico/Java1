f049496060f7800b7cc264f3be7afd4915565e05
Toshiko <kunotoshiko17@gmail.com>
2020-09-24 09:24:51 +0200

    Added test file

---
+++ lektion1/src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Toshiko Kuno
+ * Date: 2020-09-24
+ * Time: 09:24
+ * Project: IntelliJ IDEA
+ * Copyright: MIT
+ */
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
